becac10d8c1f190307233f5e163d6b1dce6ed7d2
ShubhamGoswami-1 <shubham24976@gmail.com>
2024-11-01 21:31:45 +0530

    default role admin

--- models/userModel.js
+++ models/userModel.js
@@ -19,7 +19,7 @@ const userSchema = new mongoose.Schema({
   role: {
     type: String,
     enum: ['admin', 'volunteer', 'staff'],
-    default: 'volunteer'
+    default: 'admin'
   },
   password: {
     type: String,
