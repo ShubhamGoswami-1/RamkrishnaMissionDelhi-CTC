288ac61cb611ab1c72f761035a0203d6284d16dc
ShubhamGoswami-1 <shubham24976@gmail.com>
2024-12-01 00:47:45 +0530

    removed rate limiter

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const express = require('express');
 const morgan = require('morgan');
-const rateLimit = require('express-rate-limit');
+// const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const ejs = require("ejs");
@@ -43,13 +43,13 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Limit requests from same IP
-const limiter = rateLimit({
-    max: 100,
-    windowMs: 60 * 60 * 1000,
-    message: 'Too many requests from this IP, please try again in an hour!'
-});
+// const limiter = rateLimit({
+//     max: 100,
+//     windowMs: 60 * 60 * 1000,
+//     message: 'Too many requests from this IP, please try again in an hour!'
+// });
 
-app.use('/api', limiter);
+// app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
